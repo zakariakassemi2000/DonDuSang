81f48dc4af9ae7b520e063b52ceae6b47c271de0
zakariakassemi65@gmail.com <kassemizakaria41@gmail.com>
2024-06-03 21:38:18 +0200

    le 03/06/2024

--- loadContent.js
+++ loadContent.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('./timeLigne/TimeLine.html')
+    fetch('timeline.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('imported-content').innerHTML = data;
